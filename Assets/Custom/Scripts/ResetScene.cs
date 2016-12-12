222d4dd3aa65847950b6f7fcd3d91413c84b590f
Sherry D <trilliun@users.noreply.github.com>
2016-12-11 23:56:19 -0500

    remove writing to console log

--- Assets/Custom/Scripts/ResetScene.cs
+++ Assets/Custom/Scripts/ResetScene.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 public class ResetScene : MonoBehaviour {
     public void RestartScene(string sceneName)
     {
-        Debug.Log("My method was called.");
         SceneManager.LoadScene(sceneName);
     }
 }
