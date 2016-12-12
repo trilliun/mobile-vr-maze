67406968e83f3a3d3ee88528d700de2825bcce2c
Sherry D <trilliun@users.noreply.github.com>
2016-12-11 19:17:59 -0500

    add scene restart functionality to signpost
    Allows user to reset the entire maze scene when they reach the end

---
+++ Assets/Custom/Scripts/ResetScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ResetScene : MonoBehaviour {
+    public void RestartScene(string sceneName)
+    {
+        Debug.Log("My method was called.");
+        SceneManager.LoadScene(sceneName);
+    }
+}
